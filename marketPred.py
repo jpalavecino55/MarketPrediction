5071213e6fe4799b00edfbd37c0515d9ae2cb5fb
jpalavecino55 <60577660+jpalavecino55@users.noreply.github.com>
2022-04-13 21:17:19 -0400

    Update marketPred.py

--- marketPred.py
+++ marketPred.py
@@ -1,9 +1,25 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
+import sklearn
+import math
 from sklearn.preprocessing import MinMaxScaler
 import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectKBest, f_classif, f_regression, mutual_info_regression
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+from datetime import datetime
+import plotly_express as px
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+from sklearn.svm import SVR
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
+# obtain the moving averages for the passed number of days
 def moving_average(df, days):
     for ma in days:
         new_col = "%s MA" %(str(ma))
@@ -12,6 +28,7 @@ def moving_average(df, days):
         mean = df[new_col].mean()
         df[new_col].fillna(value = mean, inplace = True)
 
+# obtain the max over the past number of passed days
 def calc_max(df, days):
     for ma in days:
         new_col = "Max over %s days" %(str(ma))
@@ -20,6 +37,7 @@ def calc_max(df, days):
         mean = df[new_col].mean()
         df[new_col].fillna(value = mean, inplace = True)
 
+# obtain the min over the past number of passed days
 def calc_min(df, days):
     for ma in days:
         new_col = "Min over %s days" %(str(ma))
@@ -28,12 +46,14 @@ def calc_min(df, days):
         mean = df[new_col].mean()
         df[new_col].fillna(value = mean, inplace = True)
 
+# normalization
 def minMaxNorm(df):
     scaler = MinMaxScaler(feature_range = (0,1))
     df = scaler.fit_transform(df)
     df = pd.DataFrame(df)
     return df
 
+# remove the null values and replace with mean
 def remove_nan(df):
     mean = df['Close'].mean()
 
@@ -77,6 +97,12 @@ aapl_df.sort_values(by=['Date'])
 amzn_df.sort_values(by=['Date'])
 msft_df.sort_values(by=['Date'])
 fb_df.sort_values(by=['Date'])
+
+tsla_df.set_index('Date', inplace=True)
+aapl_df.set_index('Date', inplace=True)
+amzn_df.set_index('Date', inplace=True)
+msft_df.set_index('Date', inplace=True)
+fb_df.set_index('Date', inplace=True)
 
 # make list of stock dataframes
 stocks = [aapl_df, amzn_df, fb_df, msft_df, tsla_df]
@@ -194,17 +220,116 @@ print(amzn_df.isnull().sum())
 print(msft_df.isnull().sum())
 print(fb_df.isnull().sum())
 
-fig = px.line(tsla_df, x='Date', y='Close')
-fig.show()
+# choose stock to test/train from
+y = aapl_df['Close'][1:]
+#y = tsla_df['Close'][1:]
+#y = amzn_df['Close'][1:]
+#y = msft_df['Close'][1:]
+#y = fb_df['Close'][1:]
 
-fig = px.line(aapl_df, x='Date', y='Close')
-fig.show()
+# rid dataset of the close data for training purposes
+aapl_df.loc[:,'Close Previous'] = aapl_df['Close']
+aapl_df = aapl_df.drop(['Close'], axis=1)
+aapl_df = aapl_df.drop(['Adj Close'], axis=1)
+#aapl_df = aapl_df.shift(periods=1)[1:]
 
-fig = px.line(amzn_df, x='Date', y='Close')
-fig.show()
+amzn_df.loc[:,'Close Previous'] = amzn_df['Close']
+amzn_df = amzn_df.drop(['Close'], axis=1)
+amzn_df = amzn_df.drop(['Adj Close'], axis=1)
+amzn_df = amzn_df.shift(periods=1)[1:]
 
-fig = px.line(msft_df, x='Date', y='Close')
-fig.show()
+msft_df.loc[:,'Close Previous'] = msft_df['Close']
+msft_df = msft_df.drop(['Close'], axis=1)
+msft_df = msft_df.drop(['Adj Close'], axis=1)
+msft_df = msft_df.shift(periods=1)[1:]
 
-fig = px.line(fb_df, x='Date', y='Close')
-fig.show()
+tsla_df.loc[:,'Close Previous'] = tsla_df['Close']
+tsla_df = tsla_df.drop(['Close'], axis=1)
+tsla_df = tsla_df.drop(['Adj Close'], axis=1)
+tsla_df = tsla_df.shift(periods=1)[1:]
+
+fb_df.loc[:,'Close Previous'] = fb_df['Close']
+fb_df = fb_df.drop(['Close'], axis=1)
+fb_df = fb_df.drop(['Adj Close'], axis=1)
+fb_df = fb_df.shift(periods=1)[1:]
+
+# split test/train
+x_train, x_test, y_train, y_test = train_test_split(aapl_df[1:], y, test_size=0.2, shuffle=False)
+
+def selectFeatures(func, k=10,x_train=x_train, y_train=y_train):
+    #apply SelectBest class to extract top 10 best features
+    bestFeat = SelectKBest(score_func=func, k=5)
+    fit = bestFeat.fit(x_train, y_train)
+    dfscores = pd.DataFrame(fit.scores_)
+    dfcolumns = pd.DataFrame(x_train.columns)
+    # concat two dataframes for better visualization
+    scores = pd.concat([dfcolumns, dfscores], axis=1)
+    scores.columns = ['Feats','Score']
+    scores.nlargest(5,'Score').plot.bar(x="Feats",y="Score",figsize=(6,5), color="Orange")
+    plt.title("Regression")
+    plt.show()
+    topTen = list(scores.nlargest(5,'Score')['Feats'])
+    for col in x_train:
+        if col not in topTen:
+            x_train.drop([col], axis=1, inplace=True)
+    return x_train
+
+x_train = selectFeatures(func=f_regression)
+x_test = x_test[x_train.columns]
+
+plt.figure(figsize=(16,8))
+sns.heatmap(x_train.corr(), cmap=plt.cm.Blues,annot=True)
+plt.title('Heatmap displaying the relationship between the features of the data', fontsize=12)
+plt.show()
+
+# mean absolute percentage error calculation
+def mape(actual, prediction):
+    actual, prediction = np.array(actual), np.array(prediction)
+    return np.mean(np.abs((actual - prediction) / actual)) * 100
+
+#regression_model = LinearRegression().fit(x_train, y_train)
+#prediction = regression_model.predict(x_test)
+#plot_df_train = pd.DataFrame(y_train)
+#plot_df = pd.DataFrame(y_test)
+#plot_df["Predictions"] = prediction
+#print(plot_df[-5:])
+#plt.figure(figsize = (18,5))
+#plt.plot(plot_df_train[-360:])
+#plt.plot(plot_df)
+#plt.show()
+#plot_df = pd.DataFrame(y_test)
+#plot_df["predictions"] = prediction
+#plot_df[-360:].plot(legend=True, figsize = (15,8))
+#plt.show()
+
+# place above into a function
+
+def train_n_test(modelName,model,x_train=x_train,x_test=x_test,y_train=y_train,y_test=y_test):
+    model=model.fit(x_train,y_train)
+    prediction = model.predict(x_test)
+    plot_df_train=pd.DataFrame(y_train)
+    plot_df=pd.DataFrame(y_test)
+    plot_df["predictions"]=prediction
+    print(plot_df[-5:])
+    plt.figure(figsize=(15,8))
+    plt.plot(plot_df_train[-360:])
+    plt.plot(plot_df)
+    plt.show()
+    plot_df=pd.DataFrame(y_test)
+    plot_df["predictions"]=prediction
+    plot_df[-360:].plot(legend=True, figsize = (15,8))
+    plt.show()
+    meanSquared = sklearn.metrics.mean_squared_error(y_test, prediction)
+    rootMeanSquared = math.sqrt(meanSquared)
+    print("Test set MAPE: {} ".format(modelName), mape(y_test, prediction))
+    print("Test set RMSE: {} ".format(modelName), rootMeanSquared)
+    return prediction
+
+regression_model = LinearRegression()
+randomForestRegressor_model = RandomForestRegressor(n_estimators=10, random_state=0)
+svr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.1))
+
+# select model to use
+regression_pred = train_n_test("Linear Regression Model", regression_model)
+#randomForest_pred = train_test_and_measure("Random Forest Regression Model", randomForestRegressor_model)
+#svr_pred = train_test_and_measure("Support Vector Regression Model", svr)
